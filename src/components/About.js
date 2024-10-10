99beb66db764d14be284d1871c6f348ccac15a11
Radha Kavade <63400955+radhakavade18@users.noreply.github.com>
2024-10-10 13:48:18 +0530

    class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,54 @@
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <p>React web series</p>
-        </div>
-    )
+import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+
+class About extends React.Component {
+    constructor() {
+        super()
+        //console.log("parent constructor")
+    }
+
+    componentDidMount() {
+        //console.log("parent componentDidMount")
+    }
+
+    render() {
+        //console.log("parent render")
+        return (
+            <div>
+                <h1>About</h1>
+                <p>React web series</p>
+                {/* <User name="Radha" location="Satara" /> */}
+                <UserClass name="Olav" location="Disney land" />
+            </div>
+        )
+    }
+
 }
 
 export default About;
+
+/*
+    the life cycle of the component
+    - when only on child rendering in parent
+        - parent constructor
+        - parent render
+        - child constructor
+        - child render
+        - child componentDidMount
+        - parent componentDidMount
+
+    - when 2 or multiple childs render in parent
+        - parent constructor
+        - parent render
+
+        - child1 constructor
+        - child1 render
+
+        - child2 constructor
+        - child2 render
+
+        - child1 componentDidMount
+        - child2 componentDidMount
+        - parent componentDidMount
+*/
